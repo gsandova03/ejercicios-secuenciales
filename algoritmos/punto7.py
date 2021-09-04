8ac4a3cb7181cfe2ba7e7f1be0c3f698aedcd34e
gabrielsr <gsrivillas0328@gmail.com>
2021-09-04 10:55:23 -0500

    Algoritmos 7 punto

---
+++ algoritmos/punto7.py
@@ -0,0 +1,6 @@
+tiempo = int( input('Tiempo trabajo en la empresa: ') )
+
+if( tiempo >= 2 ):
+  print( 'Valor del bono para el empleado $120.000 ' )
+elif( tiempo <= 1 ):
+  print( 'Valor del bono para el empleado $100.000 ' )
